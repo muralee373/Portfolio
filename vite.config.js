5d7316ff674958a642867c991a38c6761faf8f95
muralee373 <mr.midwicket@gmail.com>
2026-07-25 11:14:02 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     watch: {
+//       ignored: ['**/*.pdf']
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,6 +27,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  // GitHub Pages repository name
+  base: '/Portfolio/',
+
   server: {
     port: 3000,
     open: true,
